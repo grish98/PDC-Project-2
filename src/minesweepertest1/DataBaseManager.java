b33f1c96cb6beb615d8269e8bd57c273e3860241
Grisham Balloo <grisham98@windowslive.com>
2023-10-18 19:55:10 +1300

    EmbeddedDatabase has been created, DatabaseManager class is updated
    DataBaseManager Class is created and utilizes a singleton design pattern, so only one instance of the class can be created.

--- src/minesweepertest1/DataBaseManager.java
+++ src/minesweepertest1/DataBaseManager.java
@@ -9,6 +9,64 @@ package minesweepertest1;
  *
  * @author grish
  */
-public class DataBaseManager {
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
     
+  
+public class DataBaseManager {
+    private static final String USER_NAME = "grisham";
+    private static final String PASSWORD = "grisham";
+    private static final String URL = "jdbc:derby://localhost:1527/MineSweeperEDB; create=true";
+    
+    private Connection conn;
+
+    
+    private static DataBaseManager instance;
+
+   
+    private DataBaseManager() {
+        establishConnection();
+    }
+
+    
+    public synchronized static DataBaseManager getInstance() {
+        if (instance == null) {
+            instance = new DataBaseManager();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    private void establishConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println(URL + " connected");
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        }
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    // Step 4: Prevent cloning
+    @Override
+        public Object clone() throws
+            CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
 }
+
